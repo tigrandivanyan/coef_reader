8eb8575f839284afcc4703e23024f248d24c217b
tigrandivanyan <tigrandivanyan2004@gmail.com>
2023-08-19 14:49:26 +0000

    new coef reading

--- build/read_coefs/index.py
+++ build/read_coefs/index.py
@@ -1,36 +1,11 @@
 from glob import glob
-import easyocr
 import os
 from os import listdir
 from os.path import isfile, join
-import cv2
 import time
 import datetime
-import numpy as np
-reader = easyocr.Reader(['en'], gpu = True)
+from read_image import *
 
-def check(name):
-    if(not os.path.exists(name)):
-        path = os.path.join(name)
-        os.mkdir(path)
-
-# final
-def coefCrop(capture, no):
-    if no == "1":
-        return capture[429:487, 458:574]
-    elif no == "2":
-        return capture[437:479, 709:809]
-    elif no == "3":
-        return capture[439:480, 944:1043]
-    elif no == "4":
-        return capture[442:480, 1181:1282]
-    elif no == "5":
-        return capture[442:480, 1415:1519]
-    elif no == "6":
-        return capture[435:481, 1647:1759]
-    else:
-        return capture
-    
 def getIndexByTime(initial, time):
     if initial.minute % 2 == 0:
 
@@ -74,58 +49,10 @@ while True:
                             lines = f.readlines()
                         coefs_in_lines = list(filter(lambda line: line.__contains__("coefs"), lines))
                         if not len(coefs_in_lines) >= len(coefs):
-                            try:
-                                capture = cv2.imread(f'../scp/{race}/{capture_path}')
-                                coefText = ''
-                                check('../tmp/')
-                                for i in range(1, 7):
-                                    cv2.imwrite(f'../tmp/{i}.jpg', coefCrop(capture, str(i)))
-
-                                img_fns = sorted(glob('../tmp/*'))
-                                for img in img_fns:
-                                    coefText+=":"
-                                    image = cv2.imread(img)
-                                    average_bgr = np.mean(image[23:83, 76:186], axis=(0, 1))
-                                    if average_bgr[0]*1.5 < average_bgr[2] and average_bgr[1]*1.5 < average_bgr[2]:
-                                        coefText += "1,5"
-                                    else:
-                                        try:
-                                            coefText += reader.readtext(img)[0][1]
-                                        except:
-                                            print('err')
-                                print(coefText)
-                                with open(f'../scp/{race}/text.txt', 'a') as the_file:
-                                    the_file.write(f'coefs&{coefText}\n')
-                            except Exception as e:
-                                print(e)
+                            read_image(race, capture_path)
                     except Exception as e:
                         print(e)
-                        try:
-                            capture = cv2.imread(f'../scp/{race}/{capture_path}')
-                            coefText = ''
-                            check('../tmp/')
-                            for i in range(1, 7):
-                                cv2.imwrite(f'../tmp/{i}.jpg', coefCrop(capture, str(i)))
-
-                            img_fns = sorted(glob('../tmp/*'))
-                            for img in img_fns:
-                                print(img)
-                                coefText += ':'
-                                image = cv2.imread(img)
-                                average_bgr = np.mean(image[23:83, 76:186], axis=(0, 1))
-                                if average_bgr[0]*1.5 < average_bgr[2] and average_bgr[1]*1.5 < average_bgr[2]:
-                                    coefText += "1,5"
-                                else:
-                                    try:
-                                        coefText += reader.readtext(img)[0][1]
-                                    except:
-                                        print('err')
-                            print(coefText)
-                            with open(f'../scp/{race}/text.txt', 'a') as the_file:
-                                the_file.write(f'coefs&{coefText}\n')
-                        except Exception as e:
-                            print(e)
-        
+                        read_image(race, capture_path)
 
         scp = sorted(next(os.walk('../scp/'))[1])
     time.sleep(10)
---
+++ build/read_coefs/read_image.py
@@ -0,0 +1,112 @@
+from glob import glob
+import easyocr
+import os
+import cv2
+import numpy as np
+reader = easyocr.Reader(['en'], gpu = True)
+
+
+def coefCrop(capture, no):
+    if no == "1":
+        return capture[429:487, 458:574]
+    elif no == "2":
+        return capture[437:479, 709:809]
+    elif no == "3":
+        return capture[439:480, 944:1043]
+    elif no == "4":
+        return capture[442:480, 1181:1282]
+    elif no == "5":
+        return capture[442:480, 1415:1519]
+    elif no == "6":
+        return capture[435:481, 1647:1759]
+    else:
+        return capture
+    
+def check(name):
+    if(not os.path.exists(name)):
+        path = os.path.join(name)
+        os.mkdir(path)
+
+def read_image(race, capture_path):
+    try:
+        capture = cv2.imread(f'../scp/{race}/{capture_path}')
+        coefText = ''
+        check('../tmp/')
+        for i in range(1, 7):
+            cv2.imwrite(f'../tmp/{i}.jpg', coefCrop(capture, str(i)))
+
+        img_fns = sorted(glob('../tmp/*'))
+        for img in img_fns:
+            coefText+=":"
+            image = cv2.imread(img)
+
+            indexed_image = image
+            average_color = np.mean(indexed_image, axis=(0, 1))[::-1]
+            red = average_color[0]
+            green = average_color[1]
+            blue = average_color[2]
+
+            brightness = red + green + blue
+
+
+            # Convert to HSV color space
+            hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+            # Define a threshold for the value channel
+            brightness_threshold = brightness / 2  # Adjust this threshold value as needed
+
+            # Create a mask to filter out dark pixels
+            mask = hsv_image[:, :, 2] > brightness_threshold
+
+            # Apply the mask to the original image
+            filtered_image = image.copy()
+            filtered_image[~mask] = 0
+
+            # Calculate the average RGB values of the remaining pixels
+            remaining_pixels = filtered_image[mask]
+            average_red = np.mean(remaining_pixels[:, 2])
+            average_green = np.mean(remaining_pixels[:, 1])
+            average_blue = np.mean(remaining_pixels[:, 0])
+
+            if average_red > average_green * 1.4 and average_red > average_blue * 1.4:
+                coefText += "L"
+            elif(average_green > average_blue and average_green > average_red):
+                coefText += "H"
+            else:
+                indexed_image = image
+                average_color = np.mean(indexed_image, axis=(0, 1))[::-1]
+                red = average_color[0]
+                green = average_color[1]
+                blue = average_color[2]
+
+                print(img)
+                print(red + green + blue)
+                print(average_red + average_green + average_blue)
+
+                brightness = average_red + average_green + average_blue
+                threshVale = int(brightness / 2.3)
+
+                if threshVale > 200:
+                    threshVale = 200
+                
+                # Convert to grayscale
+                coef_crop = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                _, coef_crop = cv2.threshold(coef_crop, threshVale, 255, cv2.THRESH_BINARY)
+
+                # Apply Gaussian blur
+                coef_crop = cv2.GaussianBlur(coef_crop, (5, 5), 0)
+
+                # Apply histogram equalization for monotonicity
+                coef_crop = cv2.equalizeHist(coef_crop)
+
+                cv2.imwrite(img, coef_crop)
+                try:
+                    coefText += reader.readtext(img)[0][1]
+                except:
+                    print('err')
+
+        print(coefText)
+        with open(f'../scp/{race}/text.txt', 'a') as the_file:
+            the_file.write(f'coefs&{coefText}\n')
+    except Exception as e:
+        print(e)
